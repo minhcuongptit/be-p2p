ae4f4308fc0c6ac9ac0fee7a554b7417ef51c0b4
Trần Cường <cuongtm@vnpost.vn>
2024-08-26 17:00:09 +0700

    a

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
 
 wss.on("connection", (ws) => {
+  console.log("Client connected")
   ws.on("message", (message) => {
+    console.log(`Received message => ${message}`)
     // Gửi lại message cho tất cả các kết nối WebSocket khác
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
@@ -17,6 +19,14 @@ wss.on("connection", (ws) => {
       }
     })
   })
+  ws.on("close", () => {
+    console.log("Client disconnected")
+  })
+})
+
+// Đặt một route để kiểm tra
+app.get("/", (req, res) => {
+  res.send("Hello World!")
 })
 
 server.listen(8080, () => {
